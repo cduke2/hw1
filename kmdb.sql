ac102c90172d05294db86af7ae6289aaf64fb331
cduke2 <caroline.duke@kellogg.northwestern.edu>
2022-01-24 00:06:00 +0000

    Fix insert code

--- kmdb.sql
+++ kmdb.sql
@@ -93,8 +93,12 @@ CREATE TABLE top_cast (
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
 INSERT INTO movies (movie_title, year_released, mpaa_rating, director)
-VALUES ('Batman Begins','2005', 'PG-13', 'Christopher Nolan')
-VALUES ('The Dark Night','2008', 'PG-13', 'Christopher Nolan')
+VALUES ('Batman Begins',2005', 'PG-13', 'Christopher Nolan');
+
+INSERT INTO movies (movie_title, year_released, mpaa_rating, director)
+VALUES ('The Dark Night','2008', 'PG-13', 'Christopher Nolan');
+
+INSERT INTO movies (movie_title, year_released, mpaa_rating, director)
 VALUES ('The Dark Night Rises','2012', 'PG-13', 'Christopher Nolan');
 
 -- Prints a header for the movies output
